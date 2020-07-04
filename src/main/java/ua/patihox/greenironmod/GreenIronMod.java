047b4520b815b500bed8a7edcd1ec8349defcc8f
Eugen Torrent <39799304+PatiHox@users.noreply.github.com>
2020-07-04 14:58:03 +0300

    Ancient writings now have adequate registry name. Registry is now handled by RegistryHandler Added iron_grit and green_goo and corresponding locales. Still no textures btw ¯\_( ͡° ͜ʖ ͡°)_/¯

--- src/main/java/ua/patihox/greenironmod/GreenIronMod.java
+++ src/main/java/ua/patihox/greenironmod/GreenIronMod.java
@@ -1,25 +1,17 @@
 package ua.patihox.greenironmod;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import ua.patihox.greenironmod.init.BlockInit;
-import ua.patihox.greenironmod.init.ItemInit;
+import ua.patihox.greenironmod.registry.RegistryHandler;
 import ua.patihox.greenironmod.world.gen.ModOreGen;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("phs-greenironmod")
@@ -32,13 +24,15 @@ public class GreenIronMod
 
     public static GreenIronMod instance;
 
+    public static final GreenIronItemGroup ITEM_GROUP = new GreenIronItemGroup(ItemGroup.GROUPS.length,"phs-greenirontab");
+
     public GreenIronMod() {
         final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::setup);
         modEventBus.addListener(this::doClientStuff);
 
         instance = this;
-        MinecraftForge.EVENT_BUS.register(this);
+        RegistryHandler.init();
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -59,15 +53,13 @@ public class GreenIronMod
     }
 
     public static class GreenIronItemGroup extends ItemGroup{
-        public static final GreenIronItemGroup instance = new GreenIronItemGroup(ItemGroup.GROUPS.length,"phs-greenirontab");
-
         public GreenIronItemGroup(int index, String label) {
             super(index, label);
         }
 
         @Override
         public ItemStack createIcon() {
-            return new ItemStack(ItemInit.example_item);
+            return new ItemStack(RegistryHandler.ANCIENT_PAPER.get());
         }
     }
 
---
+++ src/main/java/ua/patihox/greenironmod/registry/RegistryHandler.java
@@ -0,0 +1,41 @@
+package ua.patihox.greenironmod.registry;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import ua.patihox.greenironmod.GreenIronMod;
+import ua.patihox.greenironmod.registry.blocks.GreenOreBlock;
+import ua.patihox.greenironmod.registry.items.BasicItem;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, GreenIronMod.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, GreenIronMod.MOD_ID);
+
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    //items
+    public static final RegistryObject<Item> ANCIENT_PAPER = ITEMS.register("ancient_paper", BasicItem::new);
+    public static final RegistryObject<Item> GREEN_GRIT = ITEMS.register("green_grit", BasicItem::new);
+    public static final RegistryObject<Item> IRON_GRIT = ITEMS.register("iron_grit", BasicItem::new);
+    public static final RegistryObject<Item> GREEN_CRYSTAL = ITEMS.register("green_crystal", BasicItem::new);
+    public static final RegistryObject<Item> GREEN_IRON = ITEMS.register("green_iron", BasicItem::new);
+    public static final RegistryObject<Item> GREEN_IRON_RAW = ITEMS.register("green_iron_raw", BasicItem::new);
+    public static final RegistryObject<Item> GREEN_GOO = ITEMS.register("green_goo", BasicItem::new);
+
+    //blocks
+    public static final RegistryObject<Block> GREEN_ORE = BLOCKS.register("green_ore", GreenOreBlock::new);
+
+    //blockItems
+    public static final RegistryObject<BlockItem> GREEN_ORE_ITEM = ITEMS.register("green_ore",
+            () -> new BlockItem(GREEN_ORE.get(),
+                new Item.Properties().group(GreenIronMod.ITEM_GROUP)));
+
+}
